b47efafc64a8730fbbdb4856750ff3218a9dbe50
João Paulo <jdjpaulo8@gmail.com>
2026-07-01 17:56:45 -0300

    feat: add MatchMarkers and render pegs dynamically

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            pegs(colors: [.red, .green, .blue, .yellow])
+            pegs(colors: [.yellow, .red, .blue, .green])
+            pegs(colors: [.red, .blue, .green, .yellow])
         }
         .padding()
+    }
+
+    func pegs(colors: [Color]) -> some View {
+        HStack {
+            ForEach(colors.indices, id: \.self) {
+                RoundedRectangle(cornerRadius: 10)
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundColor(colors[$0])
+            }
+
+            MatchMarkers(
+                matches: [.exact, .inexact, .nomatch, .exact]
+            )
+        }
     }
 }
 
---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,46 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Joao Paulo on 01/07/26.
+//
+
+import SwiftUI
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMarkers: View {
+    var matches: [Match]
+
+    var body: some View {
+        HStack {
+            VStack {
+               matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            VStack {
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func matchMarker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: { $0 == .exact})
+        let foundCount: Int = matches.count(where: { $0 != .nomatch})
+
+        Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMarkers(matches: [.exact, .inexact, .nomatch])
+}
