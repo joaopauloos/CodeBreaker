554fc460a31710d259bf26b377a933d005ff59c1
João Paulo <jdjpaulo8@gmail.com>
2026-07-23 12:02:30 -0300

    feat: create initial game model

---
+++ CodeBreaker/CodeBreaker.swift
@@ -0,0 +1,29 @@
+//
+//  CodeBreaker.swift
+//  CodeBreaker
+//
+//  Created by Joao Paulo on 23/07/26.
+//
+
+import SwiftUI
+
+typealias Peg = Color
+
+struct CodeBreaker {
+    var masterCode: Code = Code(kind: .master)
+    var guess: Code = Code(kind: .guess)
+    var attempts: [Code] = [Code]()
+    var pegChoices: [Peg] = [.red, .green, .blue, .yellow]
+}
+
+struct Code {
+    var kind: Kind
+    var pegs: [Peg] = [.red, .green, .blue, .yellow]
+
+    enum Kind {
+        case master
+        case guess
+        case attempt
+        case unknown
+    }
+}
